0619ac93d7ea8f9cead2c64349b01ae4c9a8dddb
Rohin Goyal <rohingoyal456@gmail.com>
2021-11-13 23:47:17 +0530

    Added dashboard,insert and update pages

---
+++ SQL injection UI/UI design/dashboardPage/dashboardPageDelete.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$third=$_GET['delete'];
+
+if($third=='delete'){
+    header("Location: http://localhost/sql-injection-manav/SQL%20injection%20UI/UI%20design/insertPage/insertStudentDetailPage.html");
+    exit();
+}
+
+?>
---
+++ SQL injection UI/UI design/dashboardPage/dashboardPageUpdate.php
@@ -0,0 +1,7 @@
+<?php
+$second=$_GET['update'];
+if($second=='update'){
+    header("Location: http://localhost/sql-injection-manav/SQL%20injection%20UI/UI%20design/updatePage/updateStudentDetail.html");
+    exit();
+}
+?>
